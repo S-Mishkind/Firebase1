b4f49ca952662e52f0e59b21821f340d85ec7614
Mish <mishkindstephen@gmail.com>
2021-12-11 16:04:18 -0700

    AngularFire CRUD Create - Service Implementation

--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -1,42 +1,67 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {AngularFirestore} from '@angular/fire/firestore';
-import {Course} from '../model/course';
-import {catchError, concatMap, last, map, take, tap} from 'rxjs/operators';
-import {from, Observable, throwError} from 'rxjs';
-import {Router} from '@angular/router';
-import {AngularFireStorage} from '@angular/fire/storage';
-import firebase from 'firebase/app';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Course } from "../model/course";
+import { catchError, concatMap, last, map, take, tap } from "rxjs/operators";
+import { from, Observable, throwError } from "rxjs";
+import { Router } from "@angular/router";
+import { AngularFireStorage } from "@angular/fire/storage";
+import firebase from "firebase/app";
 import Timestamp = firebase.firestore.Timestamp;
+import { CoursesServiceService } from "../Services/courses-service.service";
 
 @Component({
-  selector: 'create-course',
-  templateUrl: 'create-course.component.html',
-  styleUrls: ['create-course.component.css']
+  selector: "create-course",
+  templateUrl: "create-course.component.html",
+  styleUrls: ["create-course.component.css"],
 })
 export class CreateCourseComponent implements OnInit {
+  courseId: string;
 
   form = this.fb.group({
-    description: ["", Validators.required ],
+    description: ["", Validators.required],
     category: ["BEGINNER", Validators.required],
-    url:  ["", Validators.required ],
-    longDescription:  ["", Validators.required ],
-    promo:  [false],
-    promoStartAt:  [null]
-  })
-  constructor(private fb: FormBuilder) {
-
-  }
+    url: ["", Validators.required],
+    longDescription: ["", Validators.required],
+    promo: [false],
+    promoStartAt: [null],
+  });
+  constructor(
+    private fb: FormBuilder,
+    private coursesService: CoursesServiceService,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-
+    this.courseId = this.afs.createId();
   }
 
-  onCreateCourse(){
-    const newCourse = {...this.form.value} as Course;
+  onCreateCourse() {
+    const val = this.form.value;
+
+    const newCourse: Partial<Course> = {
+      description: val.description,
+      url: val.url,
+      longDescription: val.longDescription,
+      promo: val.promo,
+      categories: [val.category]
+    };
 
     newCourse.promoStartAt = Timestamp.fromDate(this.form.value.promoStartAt);
-    console.log(newCourse);
-  }
 
+    this.coursesService.createCourse(newCourse, this.courseId)
+    .pipe(
+      tap(course =>{
+        console.log("Created New Course", course)
+        this.router.navigateByUrl("/courses")
+      }),
+      catchError(err => {
+        console.log(err);
+        alert("Could not create the course");
+        return throwError(err);
+      })
+    )
+    .subscribe();
+  }
 }
