fb5e2ecccf20f9dca9e9f92a3541d954e7de3a00
Mish <mishkindstephen@gmail.com>
2021-12-13 05:12:32 -0700

    add updateCourse to service

--- src/app/Services/courses-service.service.ts
+++ src/app/Services/courses-service.service.ts
@@ -12,6 +12,12 @@ import { convertSnaps } from "./db-utils.service";
 export class CoursesServiceService {
   constructor(private db: AngularFirestore) {}
 
+  updateCourse(courseId: string, changes: Partial<Course>): Observable<any>{
+
+   return from(this.db.doc(`courses/${courseId}`).update(changes));
+
+  }
+
   createCourse(newCourse: Partial<Course>, courseId?: string) {
     return this.db
       .collection("courses", (ref) => ref.orderBy("seqNo", "desc").limit(1))
