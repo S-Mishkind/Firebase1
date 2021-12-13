99703666e3359f087de16a81f394089a03de6e8e
Mish <mishkindstephen@gmail.com>
2021-12-13 05:43:27 -0700

    building edit course dialog

--- src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,25 +1,36 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import { Component, Inject, OnInit } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
-import {AngularFireStorage} from '@angular/fire/storage';
-import {Observable} from 'rxjs';
-
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import { AngularFireStorage } from "@angular/fire/storage";
+import { Observable } from "rxjs";
+import { inject } from "@angular/core/testing";
 
 @Component({
-    selector: 'edit-course-dialog',
-    templateUrl: './edit-course-dialog.component.html',
-    styleUrls: ['./edit-course-dialog.component.css']
+  selector: "edit-course-dialog",
+  templateUrl: "./edit-course-dialog.component.html",
+  styleUrls: ["./edit-course-dialog.component.css"],
 })
 export class EditCourseDialogComponent {
 
-    constructor() {
+    form: FormGroup;
+    course: Course;
+  constructor(
+      private dialogRef: MatDialogRef<EditCourseDialogComponent>,
+      private fb: FormBuilder,
+      @Inject(MAT_DIALOG_DATA) course: Course,
 
-    }
+    )
+     {
+         this.course = course;
+         this.form = this.fb.group({
+             description: [course.description, Validators.required],
+             longDescription: [course.longDescription, Validators.required],
+             promo: [course.promo]
+         })
+     }
+
+  close() {
+    this.dialogRef.close();
+  }
 }
-
-
-
-
-
-
